791d60b6aed0240f828deb9b320d902556c2e301
grzracz <grz.raczek@gmail.com>
2021-07-05 21:56:57 +0200

    feat: removed unnecessary files (who needs tests anyways?)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import './index.css'
-import 'react-dayjs-picker/dist/index.css'
 import Header from './components/Header'
 import DatePickerDemo from './components/DatePickerDemo'
 
